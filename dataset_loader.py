88ddb453a49dfa78e1e44ebb7c552c9af4cf09e9
katerina2901 <113786160+katerina2901@users.noreply.github.com>
2024-03-16 17:18:59 +0300

    Add files via upload

--- dataset_loader.py
+++ dataset_loader.py
@@ -159,7 +159,7 @@ def load_dataset(name):
 
     return X, y
 
-def preprocessing(X, y, scaler=StandardScaler, test_size=0.3, random_state=42):
+def preprocessing(X, y, scaler=StandardScaler(), test_size=0.3, random_state=42):
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size,
                                        random_state=random_state, stratify=y)
