9c3f959ac1ff2a24f3ca98de0b2409291b8a29c7
ladakarimullina <150154658+ladakarimullina@users.noreply.github.com>
2024-03-19 00:30:26 +0300

    Update plot_f1.py

--- for_plotting_f1/plot_f1.py
+++ for_plotting_f1/plot_f1.py
@@ -10,24 +10,33 @@ Original file is located at
 import matplotlib.pyplot as plt
 import pandas as pd
 def plot_f1_score(file_path):
+    df_base = pd.read_csv('logidboost_baseline.csv')
+    df = pd.read_csv(file_path)
+    plt.plot(range(len(df_base)), df_base["init_F1_mean"], color='grey', linestyle=':')
+    plt.errorbar(x=range(len(df_base)), y=df_base["init_F1_mean"], 
+                 yerr=df_base["init_F1_std"], fmt='o', 
+                 color='grey', capsize=5,
+                 # transform=trans1,
+                 label='Baseline')
+    #for your csv file
+    #adjust color
+    plt.plot(range(len(df_base)), df["F1_mean"], color='green', linestyle=':')
+    plt.errorbar(x=range(len(df_base)), y=df["F1_mean"], yerr=df["F1_std"],
+                 fmt='o', color='green',
+                 capsize=5,
+                 # transform=trans2,
+                 label='With resampling')
 
-  plt.figure(figsize=(10, 8))
-  df_base = pd.read_csv('logidboost_baseline.csv')
-  df = pd.read_csv(file_path)
+    plt.xticks(range(len(df_base)), df_base['Dataset'], rotation=45, ha='right')
 
-  plt.errorbar(x=range(len(df_base)), y=df_base['F1'], yerr=df_base['std'], fmt='o', color='grey', capsize=5, label='Baseline')
-  #for your csv file
-  #adjust color
-  plt.errorbar(x=range(len(df_base)), y=df['F1'], yerr=df['std'], fmt='o', color='black', capsize=5, label='Base enstimators')
-  plt.xticks(range(len(df_base)), df_base['Dataset'], rotation=45, ha='right')
-
-  plt.title('F1 Scores by Dataset with Standard Deviation for LogidBoost')
-  plt.ylabel('F1 Score')
-  plt.xlabel('Datasets')
-  plt.legend(fontsize='12')
-  plt.gca().invert_xaxis()
-  plt.tight_layout()
-  plt.show()
+    plt.title('F1 Scores by Dataset with Standard Deviation for LogitBoost')
+    plt.ylabel('F1 Score')
+    plt.xlabel('Datasets')
+    plt.legend(fontsize='12')
+    plt.gca().invert_xaxis()
+    plt.tight_layout()
+    plt.savefig('result.png')
+    plt.show()
 
 #how to use
 #file_path = '/your_path'
